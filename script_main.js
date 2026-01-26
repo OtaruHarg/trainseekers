001016e226b2696aa37d7a4febb5147af99c2566
小樽端倶(AxolotlProject) <otarutrpg@gmail.com>
2026-01-26 17:51:18 +0900

    Update script_main.js

--- script_main.js
+++ script_main.js
@@ -1,4 +1,7 @@
 var result,key,val;
+var x = 0;
+var y = 0;
+var z = 0;
 var kintetsu = ["null","yet","yet","yet","yet","yet","yet","yet","yet","yet","yet"
              ,"yet","yet","yet","yet","yet","yet","yet","yet","yet","yet"
              ,"yet","yet","yet","yet","yet","yet","yet"];
