d1200ad3c35c87bc1a03166b4d12fbd3b5dbeb91
小樽端倶(AxolotlProject) <otarutrpg@gmail.com>
2026-01-26 09:13:33 +0900

    Create script_index.js

---
+++ script_index.js
@@ -0,0 +1,35 @@
+var pass = 0;
+var true_pass = 0;
+
+function press(x){
+    true_pass = 0;
+    pass = pass * 10;
+    while(pass >= 100000){
+        pass = pass - 100000;
+    }
+    pass = pass + x;
+    passcheck();
+}
+
+function passcheck(){
+    if(pass == 555){
+        document.querySelector("#moji").textContent = "Standing by...";
+        pass = 0;
+    }else if(pass == 0){
+        document.querySelector("#moji").textContent = "5桁のパスワードを入力してください";
+        pass = 0;
+    }
+}
+
+function press_S(){
+    true_pass = true_pass + 1;
+    if(true_pass > 5){
+        window.location.href = "main.html";
+    }
+}
+
+function press_E(){
+    document.querySelector("#moji").textContent = "ERROR";
+    pass  = 0;
+    true_pass = 0;
+}
