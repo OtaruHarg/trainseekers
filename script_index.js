123822280967c12cc16c11536763ec6b0cc7f6d3
小樽端倶(AxolotlProject) <otarutrpg@gmail.com>
2026-01-26 15:32:28 +0900

    Update script_index.js

--- script_index.js
+++ script_index.js
@@ -18,11 +18,24 @@ function passcheck(){
     }else if(pass == 0){
         document.querySelector("#moji").textContent = "5桁のパスワードを入力してください";
         pass = 0;
+    }else{
+        if(pass < 10){
+            document.querySelector("#passes").textContent = "●";
+        }else if(pass < 100){
+            document.querySelector("#passes").textContent = "●●";
+        }else if(pass < 1000){
+            document.querySelector("#passes").textContent = "●●●";
+        }else if(pass < 10000){
+            document.querySelector("#passes").textContent = "●●●●";
+        }else if(pass < 100000){
+            document.querySelector("#passes").textContent = "●●●●●";
+        }
     }
 }
 
 function press_S(){
     document.querySelector("#moji").textContent = "パスワード認証";
+    document.querySelector("#passes").textContent = "SUCCES";
     true_pass = true_pass + 1;
     window.location.href = "main.html";
     /*
@@ -33,7 +46,7 @@ function press_S(){
 }
 
 function press_E(){
-    document.querySelector("#moji").textContent = "ERROR";
+    document.querySelector("#passes").textContent = "ERROR";
     pass  = 0;
     true_pass = 0;
 }
